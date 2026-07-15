0cb92c1c670649e1ee0992a977cf57909449a3c4
Yerenson Caseres <dev.systemicflow@gmail.com>
2026-07-15 12:34:40 -0400

    Update shebang and remove code block formatting

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-```bash
+  bash
 #!/data/data/com.termux/files/usr/bin/bash
 #######################################################
 #  🎨 MOBILE DEV STUDIO - Ultimate VNC Installer v2.0
@@ -482,4 +482,4 @@ main() {
 
 # ============== RUN ==============
 main
-```
+
